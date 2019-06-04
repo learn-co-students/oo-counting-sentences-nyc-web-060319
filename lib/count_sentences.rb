f830e5e0ecf68731a1c363298ad19671da9324eb
eschechter <schechter.evan@gmail.com>
2019-06-04 15:15:52 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?('.')
   end
 
   def question?
-
+    return self.end_with?('?')
   end
 
   def exclamation?
-
+    return self.end_with?('!')
   end
 
   def count_sentences
-
+    delimiters =['.', '!', '?']
+    sentence_list = self.split(Regexp.union(delimiters))
+    return sentence_list.count do |sentence|
+      sentence != ''
+    end
   end
 end
