10e1050bf5b658dd8e5fbdbfde9cbaededfc117e
TimeSmash <TimeSmash@github.com>
2019-06-05 09:37:43 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,30 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")    
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
-  def count_sentences
-
+  def count_sentences     
+    sentence_arr = self.split(/[.!?]/)
+    if sentence_arr.include?("")
+      sentence_arr.delete("")
+    end
+    sentence_arr.length
   end
 end
