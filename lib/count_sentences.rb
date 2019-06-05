a4745127ae402421615393baa199eb35d68405d2
CindyChenx <chen_cx0128@hotmail.com>
2019-06-04 22:36:48 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,21 +15,6 @@ class String
   end
 
   def count_sentences
-    split_by_period = self.split(".")
-    split_by_question = []
-    split_by_period.each do |str| 
-      split_by_question << str.split("?")
-    end
-    split_by_question = split_by_question.flatten
-    #binding.pry
-    split_by_exclamation = []
-
-    split_by_question.each do |str|
-      split_by_exclamation << str.split("!")
-    end
-    split_by_exclamation = split_by_exclamation.flatten
-    split_by_exclamation.delete_if{|str| str.empty?()}
-    #binding.pry
-    split_by_exclamation.length
+    self.split(/\.|\?|\!/).delete_if {|w| w.size<2 }.size
   end
 end
