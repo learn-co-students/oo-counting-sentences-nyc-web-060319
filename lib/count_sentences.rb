930027a4ea74032e2bc78224156f57693a4a7e15
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-06-04 22:42:06 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      return true
+    end
+    return false
   end
 
   def question?
-
+    if self.end_with?('?')
+      return true
+    end
+    return false
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      return true
+    end
+    return false
   end
 
   def count_sentences
-
+    newArray = self.split(/\?|\.|!/)
+    newArray = newArray.reject { |e| e.length == 0 }
+    return newArray.size
+    
   end
+
 end
